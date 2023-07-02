256db915c2d96e69cc11b03820ce531b239babf9
Alena <vorozhtsova_alena@mail.ru>
2023-07-02 20:14:13 +0500

    Ура, я инициализировал переменную

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string myName;
+            myName = "Eleven";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
