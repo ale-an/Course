00671d802207081a15c14a1101951ca9a8d52d88
Alena <vorozhtsova_alena@mail.ru>
2023-07-02 21:49:52 +0500

    Анкета

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,10 +4,15 @@
     {
         public static void Main(string[] args)
         {
+            Задания();
+            Анкета();
+        }
 
-    const string MyName = "Jane";
-    
-    Console.WriteLine(MyName);
+        public static void Задания()
+        {
+            const string MyName = "Jane";
+
+            Console.WriteLine(MyName);
 
             Console.WriteLine("\t Привет Мир");
             Console.WriteLine("\t Мне 27 лет");
@@ -15,5 +20,22 @@
             Console.WriteLine("\u0023");
             Console.ReadKey();
         }
+
+        public static void Анкета()
+        {
+            const string MyName = "Alena";
+            Console.WriteLine("My name is " + MyName);
+
+            byte MyAge = 23;
+            Console.WriteLine("My age is " + MyAge);
+
+            bool HaveIApet = false;
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+
+            float MyShoeSize = 38;
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+        }
+
+
     }
 }
