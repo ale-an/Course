4e5f2c25dc852be46e66a58374ff5d84789ee095
Alena <vorozhtsova_alena@mail.ru>
2023-07-02 20:44:53 +0500

    Вывод символа

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,14 +5,14 @@
         public static void Main(string[] args)
         {
 
-            const string MyName = "Jane";
+    const string MyName = "Jane";
+    
+    Console.WriteLine(MyName);
 
-            Console.WriteLine(MyName);
-
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine("Мне 27 лет");
-            Console.WriteLine("My name is Jane");
-
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0023");
             Console.ReadKey();
         }
     }
