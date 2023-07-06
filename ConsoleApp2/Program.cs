da802a5192e9ef8bbb81ae3c0f1267def42711b9
Alena <vorozhtsova_alena@mail.ru>
2023-07-06 23:28:19 +0500

    Финальная практика модуля «Анкетируем пользователей»

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,7 @@
             Анкета();
             Цвета();
             Задание3_6_8();
+            Анкета_2();
         }
 
         public static void Задания()
@@ -48,6 +49,17 @@
             double result = 10 % 3;
             Console.WriteLine(result);
             Console.ReadKey();
+        }
+        private static void Анкета_2()
+        {
+            Console.WriteLine("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя - {0} и ваш возраст {1} ", name, age);
+            Console.WriteLine("Введите дату рождения: ");
+            var birthday = Console.ReadLine();
+            Console.WriteLine($"Дата вашего рождения - {birthday}");
         }
     }
 
