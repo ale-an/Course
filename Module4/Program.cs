04af60432eae03fd1326427c1ef08f200e97031f
Alena <vorozhtsova_alena@mail.ru>
2023-07-11 22:41:18 +0500

    Задание 4.1.18

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -10,6 +10,7 @@ namespace Module4
             Task_4_1_5();
             Task_4_1_12();
             Task_4_1_17();
+            Task_4_1_18();
         }
         private static void Task_4_1_4()
         {
@@ -67,6 +68,43 @@ namespace Module4
 
                 Console.WriteLine("Your color is cyan!");
             };
+        }
+        private static void Task_4_1_18()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+            }
         }
 
     }
