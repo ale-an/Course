397c872611051b39a932212d44fe2c455a3a0df5
Alena <vorozhtsova_alena@mail.ru>
2023-07-11 22:32:07 +0500

    Задание 4.1.17

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -9,6 +9,7 @@ namespace Module4
             Task_4_1_4();
             Task_4_1_5();
             Task_4_1_12();
+            Task_4_1_17();
         }
         private static void Task_4_1_4()
         {
@@ -36,6 +37,35 @@ namespace Module4
             else
             {
                 Console.WriteLine("Условие ложно");
+            };
+        }
+        private static void Task_4_1_17()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
             };
         }
 
