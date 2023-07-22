5e9d2c44580af646adefc722a468aa386a54ad6e
Alena <vorozhtsova_alena@mail.ru>
2023-07-22 20:37:52 +0500

    Задание 1

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello, world!");
+            Task_1();
+        }
+
+        private static void Task_1()
+        {
+            (string Name, string[] Dishes) User;
+
+            Console.Write("Введите имя пользователя ");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Введите любимое блюдо номер {i + 1}");
+                User.Dishes[i] = Console.ReadLine();
+            }
         }
     }
 }
