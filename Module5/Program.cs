e9b60b2bf182f77425d157002b2e248fa40678d3
Alena <vorozhtsova_alena@mail.ru>
2023-07-23 22:03:41 +0500

    Задание 5.2.14

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -97,15 +97,22 @@
 
         static int[] GetArrayFromConsole()
         {
-            var result = new int[5];
+            var result = GetArray();
 
-            for (int i = 0; i < result.Length; i++)
+            Sort(result);
+
+            return result;
+        }
+
+        private static int[] GetArray(int num = 5)
+        {
+            var result = new int[num];
+
+        for (int i = 0; i < result.Length; i++)
             {
                 Console.WriteLine("Введите элемент массива номер {0}", i + 1);
                 result[i] = int.Parse(Console.ReadLine());
             }
-
-            Sort(result);
 
             return result;
         }
