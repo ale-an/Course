931c5fde20b83811046c16197863fa5534d4adc6
Alena <vorozhtsova_alena@mail.ru>
2023-07-31 20:54:23 +0500

    Задание 6.3.2

---
+++ Module6/Bus.cs
@@ -0,0 +1,18 @@
+﻿namespace Module6;
+
+public class Bus
+{
+    public int? Load;
+
+    public void PrintStatus()
+    {
+        if (Load.HasValue && Load > 0)
+        {
+            Console.WriteLine($"В автобусе {Load.Value} пассажиров");
+        }
+        else
+        {
+            Console.WriteLine("Автобус пуст!");
+        }
+    }
+}
