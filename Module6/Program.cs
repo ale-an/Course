f14fca6e8b252b01973a35fdb30bbad23a09c27e
Alena <vorozhtsova_alena@mail.ru>
2023-07-31 18:57:28 +0500

    Задание 6.2.2

---
+++ Module6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Чёрный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
