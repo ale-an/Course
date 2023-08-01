7460df79b3194fbe18815e49294fa4dc82e8c936
Alena <vorozhtsova_alena@mail.ru>
2023-08-01 21:13:20 +0500

    Задание 6.5.2

---
+++ Module6/Figures.cs
@@ -0,0 +1,46 @@
+﻿namespace Module6;
+
+class Triangle
+{
+    public double a;
+    public double b;
+    public double c;
+
+    public double Perimeter()
+    {
+        return 0;
+    }
+
+    public double Area()
+    {
+        return 0;
+    }
+}
+class Circle
+{
+    public double r;
+
+    public double Perimeter()
+    {
+        return 0;
+    }
+
+    public double Area()
+    {
+        return 0;
+    }
+} 
+class Square
+{
+    public double r;
+
+    public double Perimeter()
+    {
+        return 0;
+    }
+
+    public double Area()
+    {
+        return 0;
+    }
+}
