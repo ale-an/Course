04be4a29aa37ca4e98632132081a07b2f7184100
Alena <vorozhtsova_alena@mail.ru>
2023-08-02 21:03:40 +0500

    Задание 6.6.2

---
+++ Module6/User.cs
@@ -0,0 +1,60 @@
+﻿namespace Module6;
+
+public class User
+{
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+
+        set
+        {
+            if (value < 18)
+            {
+                Console.WriteLine("Возраст должен быть не меньше 18");
+            }
+            else
+            {
+                age = value;
+            }
+        }
+    }
+
+    private string login;
+
+    public string Login
+    {
+        get { return login; }
+
+        set
+        {
+            if (value.Length < 3)
+            {
+                Console.WriteLine("Логин должен быть не менее 3 символов");
+            }
+            else
+            {
+                login = value;
+            }
+        }
+    }
+    private string email;
+
+    public string Email
+    {
+        get { return email; }
+
+        set
+        {
+            if (value.Contains('@'))
+            {
+                email = value;
+            }
+            else
+            {
+                Console.WriteLine("В электронной почте должен присутствовать символ @");
+            }
+        }
+    }
+}
