53f31b9496db382141da11d808eb64dacf8f419f
Alena <vorozhtsova_alena@mail.ru>
2023-08-04 21:52:20 +0500

    Задание 7.1.4

---
+++ Module7/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace Module7;
+
+public class Employee
+{
+    public string Name;
+    public int Age;
+    public int Salary;
+}
+
+class ProjectManager : Employee
+{
+    public string ProjectName;
+}
+
+class Developer : Employee
+{
+    public string ProgrammingLanguage;
+}
---
+++ Module7/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Module7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
