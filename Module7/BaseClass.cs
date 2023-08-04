fd4603fd32300b01b44bd04c2d8ffba4a295c0ec
Alena <vorozhtsova_alena@mail.ru>
2023-08-04 22:10:15 +0500

    Задание 7.1.10

---
+++ Module7/BaseClass.cs
@@ -0,0 +1,29 @@
+﻿namespace Module7;
+
+class BaseClass
+{
+    protected string Name;
+
+    public BaseClass(string name)
+    {
+        Name = name;
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public string Description;
+
+    public int Counter;
+
+    public DerivedClass(string name, string description) : base(name)
+    {
+        Description = description;
+    }
+
+    public DerivedClass(string name, string description, int counter) : base(name)
+    {
+        Description = description;
+        Counter = counter;
+    }
+}
