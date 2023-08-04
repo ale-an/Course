af00c06ee512c38d46a36840c5df58f83d3fb55f
Alena <vorozhtsova_alena@mail.ru>
2023-08-04 21:56:31 +0500

    Задание 7.1.5

---
+++ Module7/Food.cs
@@ -0,0 +1,33 @@
+﻿namespace Module7;
+
+public class Food
+{
+}
+
+class Fruit : Food
+{
+}
+
+class Vegetable : Food
+{
+}
+
+class Apple : Fruit
+{
+}
+
+class Banana : Fruit
+{
+}
+
+class Pear : Fruit
+{
+}
+
+class Potato : Vegetable
+{
+}
+
+class Carrot : Vegetable
+{
+}
