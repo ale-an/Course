f55919f5e5493def8ac3bc6d49d8c3364746cfc8
Alena <vorozhtsova_alena@mail.ru>
2023-08-04 22:02:12 +0500

    Задание 7.1.6

---
+++ Module7/Obj.cs
@@ -0,0 +1,17 @@
+﻿namespace Module7;
+
+public class Obj
+{
+    private string name;
+    private string owner;
+    private int length;
+    private int count;
+
+    public Obj(string name, string ownerName, int objLength, int count)
+    {
+        this.name = name;
+        owner = ownerName;
+        length = objLength;
+        this.count = count;
+    }
+}
