1bba1b7c03c43754c2e338b7d9391c828750fe81
Alena <vorozhtsova_alena@mail.ru>
2023-08-07 22:16:57 +0500

    Задание 7.2.7

---
+++ Module7/A.cs
@@ -0,0 +1,41 @@
+﻿namespace Module7;
+
+public class A
+{
+    public virtual void Display()
+    {
+        Console.WriteLine("A");
+    }
+}
+
+public class B : A
+{
+    public new void Display()
+    {
+        Console.WriteLine("B");
+    }
+}
+
+public class C : A
+{
+    public override void Display()
+    {
+        Console.WriteLine("C");
+    }
+}
+
+public class D : B
+{
+    public new void Display()
+    {
+        Console.WriteLine("D");
+    }
+}
+
+public class E : C
+{
+    public new void Display()
+    {
+        Console.WriteLine("E");
+    }
+}
