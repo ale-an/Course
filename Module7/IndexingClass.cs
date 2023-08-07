af2091e08f2caee93333957a4b48abc9489c328e
Alena <vorozhtsova_alena@mail.ru>
2023-08-07 22:54:09 +0500

    Задание 7.2.14

---
+++ Module7/IndexingClass.cs
@@ -0,0 +1,34 @@
+﻿namespace Module7;
+
+public class IndexingClass
+{
+    private int[] array;
+
+    public IndexingClass(int[] array)
+    {
+        this.array = array;
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index >= 0 && index < array.Length)
+            {
+                return array[index];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private set
+        {
+            if (index >= 0 && index < array.Length)
+            {
+                array[index] = value;
+            }
+        }
+    }
+}
