dbcd1fcefba3a435690434460d7f84c3f05512ac
Alena <vorozhtsova_alena@mail.ru>
2023-08-07 22:43:14 +0500

    Задание 7.2.12

---
+++ Module7/Obj2.cs
@@ -0,0 +1,22 @@
+﻿namespace Module7;
+
+public class Obj2
+{
+    public int Value;
+
+    public static Obj2 operator +(Obj2 source, Obj2 target)
+    {
+        return new Obj2()
+        {
+            Value = source.Value + target.Value
+        };
+    }
+    
+    public static Obj2 operator -(Obj2 source, Obj2 target)
+    {
+        return new Obj2()
+        {
+            Value = source.Value - target.Value
+        };
+    }
+}
