04a0bb75808723c910cd3501884e6653438b2ab7
Alena <vorozhtsova_alena@mail.ru>
2023-08-08 19:41:41 +0500

    Задание 7.5.3

---
+++ Module7/Helper.cs
@@ -0,0 +1,11 @@
+﻿namespace Module7;
+
+public class Helper
+{
+    static void Swap(ref int num1, ref int num2)
+    {
+        int num3 = num1;
+        num1 = num2;
+        num2 = num3;
+    }
+}
