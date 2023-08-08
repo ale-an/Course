7a17134b6b6efe02815207c2a4d8c9af7c8b776f
Alena <vorozhtsova_alena@mail.ru>
2023-08-08 20:11:44 +0500

    Задание 7.5.9

---
+++ Module7/IntExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Module7;
+
+public static class IntExtensions
+{
+    public static int GetNegative(this int source)
+    {
+        return source > 0 ? source * -1 : source;
+    }
+    public static int GetPositive(this int source)
+    {
+        return source < 0 ? source * -1 : source;
+    }
+}
