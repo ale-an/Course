f90c2679552417b06c286296270c250b74b4873c
Alena <vorozhtsova_alena@mail.ru>
2023-08-14 18:39:39 +0500

    Задание 8.1.4

---
+++ Module8/Disk.cs
@@ -0,0 +1,15 @@
+﻿namespace Module8;
+
+public class Disk
+{
+    public string Name { get; set; }
+    public double Capacity { get; set; }
+    public double FreeSpace { get; set; }
+
+    public Disk(string name, double capacity, double freeSpace)
+    {
+        Name = name;
+        Capacity = capacity;
+        FreeSpace = freeSpace;
+    }
+}
