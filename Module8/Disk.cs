f29ed7657e05f4c79a5d542567b43f40cc7b5c93
Alena <vorozhtsova_alena@mail.ru>
2023-08-14 19:03:33 +0500

    Задание 8.1.5

--- Module8/Disk.cs
+++ Module8/Disk.cs
@@ -5,11 +5,22 @@ public class Disk
     public string Name { get; set; }
     public double Capacity { get; set; }
     public double FreeSpace { get; set; }
+    
+    public List<Folder> Folders { get; set; } = new List<Folder>();
 
     public Disk(string name, double capacity, double freeSpace)
     {
         Name = name;
         Capacity = capacity;
         FreeSpace = freeSpace;
+    }
+
+    public void CreateFolder(string name)
+    {
+        Folders.Add(new Folder()
+        {
+            Name = name
+        });
+        Console.WriteLine("Папка создана");
     }
 }
---
+++ Module8/Folder.cs
@@ -0,0 +1,7 @@
+﻿namespace Module8;
+
+public class Folder
+{
+    public string Name { get; set; }
+    public List<string> Files { get; set; } = new List<string>();
+}
