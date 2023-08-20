b9b20597f9a5a32731b56fcc7cd3d35e17d3d593
Alena <vorozhtsova_alena@mail.ru>
2023-08-20 15:49:15 +0500

    Задание 9.2.2

--- Module9/Program.cs
+++ Module9/Program.cs
@@ -4,9 +4,31 @@
     {
         static void Main(string[] args)
         {
+            Task_9_1_4();
+            Task_9_2_2();
+        }
+
+        static void Task_9_1_4()
+        {
             Exception exception = new Exception("Всё плохо. Но дело не в Вас...");
             exception.Data.Add("Дата создания исключения : ", DateTime.Now);
             exception.HelpLink = "www.bing.com";
+        }
+
+        private static void Task_9_2_2()
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException($"Произошла ошибка!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
         }
     }
 }
