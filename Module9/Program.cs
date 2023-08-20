59432c7fc553a5a9624d86196cf04cfef7e8f5b4
Alena <vorozhtsova_alena@mail.ru>
2023-08-20 15:08:54 +0500

    Задание 9.1.3

---
+++ Module9/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception();
+            exception.Data.Add("Дата создания исключения : ", DateTime.Now);
+        }
+    }
+}
