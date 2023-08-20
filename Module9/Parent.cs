773341c87741bda2eb959ca107d8cd740440640a
Alena <vorozhtsova_alena@mail.ru>
2023-08-20 18:46:19 +0500

    Задание 9.4.3

---
+++ Module9/Parent.cs
@@ -0,0 +1,38 @@
+﻿namespace Module9;
+
+class Program2
+{
+    public delegate Parent HandlerMethod();
+
+    delegate void ChildInfo(Child child);
+
+    public static Parent ParentHandler()
+    {
+        return null;
+    }
+
+    public static Child ChildHandler()
+    {
+        return null;
+    }
+
+    static void Main2(string[] args)
+    {
+        ChildInfo childInfo = GetParentInfo;
+        childInfo.Invoke(new Child());
+        Console.Read();
+    }
+
+    public static void GetParentInfo(Parent g)
+    {
+        Console.WriteLine(g.GetType());
+    }
+}
+
+public class Parent
+{
+}
+
+public class Child : Parent
+{
+}
