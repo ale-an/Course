cc7d62b2e8244948aa3cda52bb6dab4fcd7e8ac5
Alena <vorozhtsova_alena@mail.ru>
2023-08-20 15:12:58 +0500

    Задание 9.1.4

--- Module9/Program.cs
+++ Module9/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Exception exception = new Exception();
+            Exception exception = new Exception("Всё плохо. Но дело не в Вас...");
             exception.Data.Add("Дата создания исключения : ", DateTime.Now);
+            exception.HelpLink = "www.bing.com";
         }
     }
 }
