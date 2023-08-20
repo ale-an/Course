2523e0ed6c1e05d290c49035fa7b3de128e9cad4
Alena <vorozhtsova_alena@mail.ru>
2023-08-20 18:27:52 +0500

    Задание 9.4.2

---
+++ Module9/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace Module9;
+
+class Program1
+{
+    public delegate Car HandlerMethod();
+
+    public static Car CarHandler()
+    {
+        return null;
+    }
+
+    public static Lexus LexusHandler()
+    {
+        return null;
+    }
+
+    static void Main1(string[] args)
+    {
+        HandlerMethod handlerCar = LexusHandler;
+        Console.Read();
+    }
+
+    public class Car
+    {
+    }
+
+    public class Lexus : Car
+    {
+    }
+}
