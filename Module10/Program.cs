3fa8c1e8ce40d7a9a699a8644848e938028f336f
Alena <vorozhtsova_alena@mail.ru>
2023-08-27 21:01:19 +0500

    Задание 10.1.4

---
+++ Module10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Module10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface IManager
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();
+    }
+
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+        }
+
+        public void Read()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Delete()
+        {
+        }
+    }
+}
