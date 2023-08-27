59c5d240921e31c6b01495891f27f77a40b5faca
Alena <vorozhtsova_alena@mail.ru>
2023-08-27 21:36:21 +0500

    Задание 10.3.1

---
+++ Module10/FileManager.cs
@@ -0,0 +1,28 @@
+﻿namespace Module10;
+
+public class FileManager : IWriter, IReader, IMailer
+{
+    public void Write()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Read()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendEmail()
+    {
+        throw new NotImplementedException();
+    }
+}
+public interface IReader 
+{
+    void Read();
+}
+
+public interface IMailer 
+{
+    void SendEmail();
+}
