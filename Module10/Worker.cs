2580405d47a3465933d0781e4ddd4ea3b2be74b0
Alena <vorozhtsova_alena@mail.ru>
2023-08-27 21:23:00 +0500

    Задание 10.2.3

---
+++ Module10/Worker.cs
@@ -0,0 +1,14 @@
+﻿namespace Module10;
+
+public class Worker : IWorker
+{
+    public void Build()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IWorker
+{
+    public void Build();
+}
