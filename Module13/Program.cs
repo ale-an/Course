e85ae3d102957aa21fd70a9ec452976639030336
Alena <vorozhtsova_alena@mail.ru>
2023-09-20 20:20:43 +0500

    Задание 13.1.4

---
+++ Module13/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Module13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_13_1_4(new[] {1, 2, 3});
+        }
+
+        private static bool Task_13_1_4(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
