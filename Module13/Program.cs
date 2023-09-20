55a62b1e403b2352edbc4dbc37c5bdff056fb8fd
Alena <vorozhtsova_alena@mail.ru>
2023-09-20 21:03:12 +0500

    Задание 13.1.6

--- Module13/Program.cs
+++ Module13/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Task_13_1_4(new[] {1, 2, 3});
+            Task_13_1_6();
         }
 
         private static bool Task_13_1_4(int[] array)
@@ -18,6 +19,34 @@
             }
 
             return true;
+        }
+
+        private static void Task_13_1_6()
+        {
+            string text;
+            using (var sr = File.OpenText(@"C:\Users\tipoy\Downloads\book.txt"))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            var count = 0;
+            var currentWord = "";
+
+            foreach (var symbol in text)
+            {
+                if (symbol is ' ' or '\r' or '\n')
+                {
+                    if (currentWord.Length == 0)
+                        continue;
+
+                    currentWord = "";
+                    count++;
+                }
+                else
+                    currentWord += symbol;
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
