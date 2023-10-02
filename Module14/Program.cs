8410c949be237c870002841f941e8a2efb0d5e7b
Alena <vorozhtsova_alena@mail.ru>
2023-10-02 21:24:13 +0500

    Задание 4

---
+++ Module14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+
+namespace Module14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_4();
+        }
+
+        private static void Task_4()
+        {
+            string[] people = {"Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян"};
+            var list = new List<string>();
+            foreach (var person in people)
+            {
+                if (person[0] == 'А')
+                {
+                    list.Add(person);
+                }
+            }
+
+            list.Sort();
+        }
+    }
+}
