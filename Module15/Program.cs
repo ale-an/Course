f9a5c0096a989f0091d7da045412fb58258a3b2b
Alena <vorozhtsova_alena@mail.ru>
2023-10-08 14:54:09 +0500

    Задание 15.2.2

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -8,6 +8,7 @@
             Task_15_1_5();
             Task_15_1_6();
             Task_15_2_1(5);
+            Task_15_2_2();
         }
 
         private static void Task_15_1_4()
@@ -74,6 +75,24 @@
             }
 
             return numbers.Aggregate((x, y) => x * y);
+        }
+
+        private static void Task_15_2_2()
+        {
+            var contacts = new List<Contact>()
+            {
+                new Contact() {Name = "Андрей", Phone = 79994500508},
+                new Contact() {Name = "Сергей", Phone = 799990455},
+                new Contact() {Name = "Иван", Phone = 79999675334},
+                new Contact() {Name = "Игорь", Phone = 8884994},
+                new Contact() {Name = "Анна", Phone = 665565656},
+                new Contact() {Name = "Василий", Phone = 3434}
+            };
+
+            var incorrectContacts =
+                contacts.Count(s => s.Phone.ToString().Length != 11 || s.Phone.ToString()[0] != '7');
+
+            Console.WriteLine($"Неверных номеров телефонов: {incorrectContacts}");
         }
     }
 }
