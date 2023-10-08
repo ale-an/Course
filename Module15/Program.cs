2ff6954959564193a9ebd540e97872559a4ebcf9
Alena <vorozhtsova_alena@mail.ru>
2023-10-08 13:50:48 +0500

    Задание 15.1.4

---
+++ Module15/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Module15
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_15_1_4();
+        }
+
+        private static void Task_15_1_4()
+        {
+            Console.WriteLine("Введите первое слово");
+            var word1 = Console.ReadLine();
+            Console.WriteLine("Введите второе слово");
+            var word2 = Console.ReadLine();
+            var sameSymbol = word2.Intersect(word1);
+
+            foreach (var s in sameSymbol)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
