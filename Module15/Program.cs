094584b256e4c578636d4448e39c51f1a53784fb
Alena <vorozhtsova_alena@mail.ru>
2023-10-08 14:18:04 +0500

    Задание 15.1.6 и поправила форматирование

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -6,14 +6,17 @@
         {
             Task_15_1_4();
             Task_15_1_5();
+            Task_15_1_6();
         }
 
         private static void Task_15_1_4()
         {
             Console.WriteLine("Введите первое слово");
             var word1 = Console.ReadLine();
+
             Console.WriteLine("Введите второе слово");
             var word2 = Console.ReadLine();
+
             var sameSymbol = word2.Intersect(word1);
 
             foreach (var s in sameSymbol)
@@ -28,7 +31,7 @@
             {
                 "Microsoft", "Apple", "Oracle"
             };
- 
+
             var hardwareManufacturers = new List<string>()
             {
                 "Apple", "Samsung", "Intel"
@@ -40,6 +43,24 @@
             {
                 Console.WriteLine(c);
             }
+        }
+
+        private static void Task_15_1_6()
+        {
+            char[] marks = {' ', ',', '.', ';', ':', '?', '!'};
+
+            Console.WriteLine("Напишите что-нибудь");
+            var suggestion = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(suggestion))
+            {
+                Console.WriteLine("Попытайтесь ещё");
+                return;
+            }
+
+            var symbols = suggestion.Except(marks).Distinct().Count();
+
+            Console.WriteLine($"Уникальных символов: {symbols}");
         }
     }
 }
