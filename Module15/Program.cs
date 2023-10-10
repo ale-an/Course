7e72c1dcb0698c31cf2854f5a7bf7fcf33285bfe
Alena <vorozhtsova_alena@mail.ru>
2023-10-10 22:16:29 +0500

    Задание 15.4.2

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -15,6 +15,7 @@ namespace Module15
             Task_15_2_8();
             Task_15_3_3();
             Task_15_4_1();
+            Task_15_4_2();
         }
 
         private static void Task_15_1_4()
@@ -197,6 +198,42 @@ namespace Module15
             foreach (var elem in result)
             {
                 Console.WriteLine($"{elem.Name} - {elem.DepartmentName}");
+            }
+        }
+
+        private static void Task_15_4_2()
+        {
+            var departments = new List<Department>()
+            {
+                new Department() {Id = 1, Name = "Программирование"},
+                new Department() {Id = 2, Name = "Продажи"}
+            };
+
+            var employees = new List<Employee>()
+            {
+                new Employee() {DepartmentId = 1, Name = "Инна", Id = 1},
+                new Employee() {DepartmentId = 1, Name = "Андрей", Id = 2},
+                new Employee() {DepartmentId = 2, Name = "Виктор ", Id = 3},
+                new Employee() {DepartmentId = 3, Name = "Альберт ", Id = 4},
+            };
+
+            var result = departments.GroupJoin(employees,
+                department => department.Id,
+                emp => emp.DepartmentId,
+                (d, e) =>
+                    new
+                    {
+                        Employees = e,
+                        DepartmentName = d.Name
+                    });
+
+            foreach (var group in result)
+            {
+                Console.WriteLine(group.DepartmentName + ":");
+                foreach (var employ in group.Employees)
+                {
+                    Console.WriteLine(employ.Name);
+                }
             }
         }
     }
